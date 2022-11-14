0895d592d9fcad30f821af46ad384804d042bc63
EricchiYukia <git@ericchi.moe>
2022-11-14 17:06:20 +0100

    Fatto fino al 4

---
+++ jsnack4/script.js
@@ -0,0 +1,18 @@
+function func() {
+    let invitati = ['sayori', 'yuri', 'natsuki', 'monika'];
+    let nome = document.getElementById("nome").value;
+    let invito = false;
+
+    for (let i = 0; i < nome.length ; i++){
+        if (nome.toLowerCase() == invitati[i]){
+            invito = true;
+            break;
+        }
+    }
+
+    if(invito == true) {
+        alert("Benvenutə al Club di Letteratura!");
+    } else {
+        alert("Just Monika.");
+    }
+}
